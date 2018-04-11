3d0678812155ac8e5944fef8aebb615745d64922
ryo arata <arata@v-force.co.jp>
2018-04-12 07:41:09 +0900

    jsfix

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,9 @@ class RoomsController < ApplicationController
   def index
   	matthings = current_user.profile.gender? ? current_user.woman_mattchings : current_user.man_mattchings
   	@users = current_user.profile.gender? ? matthings.map{|m| m.man } : matthings.map{|m| m.woman }
+  	@users = [] if @users.blank?
   	@today_user = @users.map{|u| u unless u.tickets.where(end: false).blank? }.compact!
+  	@today_user = [] if @today_user.blank?
   end
 
   def show
