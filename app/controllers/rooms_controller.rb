ab22f184857183b32d47b0eb0ba8d3b1ec415bcf
ryo arata <arata@v-force.co.jp>
2018-04-12 06:33:30 +0900

    asset:precompile

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,7 @@ class RoomsController < ApplicationController
   def index
   	matthings = current_user.profile.gender? ? current_user.woman_mattchings : current_user.man_mattchings
   	@users = current_user.profile.gender? ? matthings.map{|m| m.man } : matthings.map{|m| m.woman }
-  	@today_go = @users.map{|u| u unless u.tickets.where(end: false).blank? }
+  	@today_user = @users.map{|u| u unless u.tickets.where(end: false).blank? }.compact!
   end
 
   def show
