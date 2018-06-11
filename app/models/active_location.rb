fcd4e6dc07f4136ae661424b96a0945ef185176e
ryo arata <arata@v-force.co.jp>
2018-06-11 16:43:57 +0900

    active locationを選択式に

---
+++ app/models/active_location.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: food_styles
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class ActiveLocation < ActiveRecord::Base
+	has_many :profiles
+end
